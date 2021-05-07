ac85e7c7328d4a57a767169b5d2042c70577f0c6
Simon Ingelsson <simon.ingelsson@utopiamusic.com>
2021-05-07 11:44:20 +0200

    fix shellsheck

--- scripts/_dm-helper.sh
+++ scripts/_dm-helper.sh
@@ -3,7 +3,8 @@
 
 get_config() {
   local config_dirs=() 
-  local _local_conf="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo ".")")./config" && pwd)"
+  local _local_conf
+  _local_conf="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo ".")")./config" && pwd)"
   if [[ -f "${_local_conf}/config" ]]; then
     config_dirs+=( "${_local_conf}/config"  )
   else 
@@ -16,7 +17,6 @@ get_config() {
     if [[ -f ${conf} ]]; then
       echo "${conf}"
       return
-      loaded=1
       break
     fi
   done
