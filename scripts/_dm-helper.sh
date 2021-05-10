d53a6ee0d783b831ea3d8d53a5f86d35ef8bd637
Simon Ingelsson <simon.ingelsson@utopiamusic.com>
2021-05-10 13:12:07 +0200

    added diff check (only visible in dm-hub atm)

--- scripts/_dm-helper.sh
+++ scripts/_dm-helper.sh
@@ -16,18 +16,15 @@ if [[ ${sourced} -eq 0 ]]; then
 fi
 
 get_config() {
-  local config_dirs=() 
+  local _config_files=() 
   local _local_conf
   _local_conf="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo ".")")./config" && pwd)"
-  if [[ -f "${_local_conf}/config" ]]; then
-    config_dirs+=( "${_local_conf}/config"  )
-  else 
-    config_dirs+=(
-    "${HOME}/.config/dmscripts/config"
-    "/etc/dmscripts/config"
-    )
-  fi
-  for conf in "${config_dirs[@]}"; do
+
+  _config_files+=( "${HOME}/.config/dmscripts/config" )
+  [[ -f "${_local_conf}/config" ]] && _config_files+=( "${_local_conf}/config" )
+  _config_files+=( "/etc/dmscripts/config" )
+
+  for conf in "${_config_files[@]}"; do
     if [[ -f ${conf} ]]; then
       echo "${conf}"
       return
@@ -36,3 +33,24 @@ get_config() {
   done
 }
 
+# Check if config has updates that should be displayed to the user
+check_updated_config() {
+  local _base_file
+  local _config_file
+  _base_file=-1
+  [[ -f /etc/dmscripts/config ]] && _base_file="/etc/dmscripts/config"
+  _local_conf="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo ".")")./config" && pwd)"
+  [[ -f "${_local_conf}/config" ]] && _base_file=${_local_conf}/config
+  _config_file=$(get_config)
+
+  [[ "${_config_file}" == "${_base_file}" ]] && return 
+
+  _config_file_revision=$(grep "^_revision=" "${_config_file}")
+  _base_file_revision=$(grep "^_revision=" "${_base_file}")
+
+  if [[ ! "${_config_file_revision}" == "${_base_file_revision}" ]] ; then
+    diff -y "${_config_file}" "${_base_file}" | less
+    echo  "${_config_file}  > ${_base_file}"
+    echo  "New revision of the configuration detected, please review and set ${_base_file_revision} in ${_config_file} when done"
+  fi
+}
